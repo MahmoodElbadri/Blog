970b17307af03ca4102ea7bfebc33d54fb2fa7f2
MahmoodElbadri <mahmoud.elbadry357@gmail.com>
2025-04-12 16:20:57 +0200

    Update to .NET 9.0 with EF Core and SQL Server support
    - Upgraded project to .NET 9.0 and added Entity Framework Core.
    - Added package references for `Microsoft.EntityFrameworkCore.SqlServer` and `Microsoft.EntityFrameworkCore.Tools`.
    - Created `ApplicationDbContext` to manage `BlogPost` and `Tag` entities.
    - Configured SQL Server connection in `appsettings.json`.
    - Added initial migration for `Posts`, `Tags`, and `BlogPostTag` tables.
    - Generated model snapshot for current database schema state.

---
+++ Blog.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Blog.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Web.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<BlogPost> Posts { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+}
