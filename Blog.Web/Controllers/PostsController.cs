52f1fd85ea70612995a2bb9114b067a4b1a42c31
MahmoodElbadri <mahmoud.elbadry357@gmail.com>
2025-04-15 01:08:23 +0200

    Add post management functionality and UI updates
    - Introduced `PostsController` with `Index` and `Add` actions for managing blog posts.
    - Created `PostAddRequest` view model to encapsulate post data.
    - Updated `_Layout.cshtml` to include a link for adding posts and revamped footer design.
    - Added `Add.cshtml` view for post creation with a form for entering post details and selecting tags.

---
+++ Blog.Web/Controllers/PostsController.cs
@@ -0,0 +1,43 @@
+﻿using Blog.Web.Models.ViewModels;
+using Blog.Web.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Threading.Tasks;
+
+namespace Blog.Web.Controllers
+{
+    public class PostsController : Controller
+    {
+        private readonly ITagRepository _tagRepository;
+
+        public PostsController(ITagRepository tagRepository)
+        {
+            this._tagRepository = tagRepository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            var tags = await _tagRepository.GetAllTagsAsync();
+            var model = new PostAddRequest
+            {
+                Tags = tags.Select(tmp => new SelectListItem
+                {
+                    Text = tmp.DisplayName,
+                    Value = tmp.ID.ToString(),
+                })
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Add(PostAddRequest request)
+        {
+            return RedirectToAction("Add");
+        }
+    }
+}
---
+++ Blog.Web/Models/ViewModels/PostAddRequest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Blog.Web.Models.ViewModels;
+
+public class PostAddRequest
+{
+    public string Header { get; set; }
+    public string PageTitle { get; set; }
+    public string Content { get; set; }
+    public string ShortDescription { get; set; }
+    public string FeaturedImageUrl { get; set; }
+    public string UrlHandle { get; set; }
+    public DateTime PublishedDate { get; set; }
+    public string Author { get; set; }
+    public bool IsVisible { get; set; }
+    public IEnumerable<SelectListItem> Tags { get; set; }
+    public string SelectedTag { get; set; }
+}
