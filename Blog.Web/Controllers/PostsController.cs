c681f9285d3589bdcd73563ebfcf7563eb3033ce
MahmoodElbadri <mahmoud.elbadry357@gmail.com>
2025-04-19 13:48:02 +0200

    Enhance post management and validation features
    Updated PostsController to manage blog posts asynchronously with IPostRepository. Enhanced PostAddRequest with validation attributes and added Tags property. Modified Add.cshtml to streamline UI and adjusted JavaScript for dropdown behavior.

--- Blog.Web/Controllers/PostsController.cs
+++ Blog.Web/Controllers/PostsController.cs
@@ -1,4 +1,5 @@
-﻿using Blog.Web.Models.ViewModels;
+﻿using Blog.Web.Models.Domain;
+using Blog.Web.Models.ViewModels;
 using Blog.Web.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -9,10 +10,12 @@ namespace Blog.Web.Controllers
     public class PostsController : Controller
     {
         private readonly ITagRepository _tagRepository;
+        private readonly IPostRepository _postRepository;
 
-        public PostsController(ITagRepository tagRepository)
+        public PostsController(ITagRepository tagRepository, IPostRepository postRepository)
         {
             this._tagRepository = tagRepository;
+            this._postRepository = postRepository;
         }
         public IActionResult Index()
         {
@@ -35,8 +38,33 @@ namespace Blog.Web.Controllers
         }
 
         [HttpPost]
-        public IActionResult Add(PostAddRequest request)
+        public async Task<IActionResult> Add(PostAddRequest request)
         {
+            var post = new BlogPost
+            {
+                Author = request.Author,
+                Content = request.Content,
+                FeaturedImageUrl = request.FeaturedImageUrl,
+                Header = request.Header,
+                IsVisible = request.IsVisible,
+                PageTitle = request.PageTitle,
+                PublishedDate = request.PublishedDate,
+                ShortDescription = request.ShortDescription,
+                UrlHandle = request.UrlHandle,
+            };
+            var selectedTags = new List<Tag>();
+            foreach (var selectedTagId in request.SelectedTags)
+            {
+                var selectedTagIdAsGuid = Guid.Parse(selectedTagId);
+                var existingTag = await _tagRepository.GetByIdAsync(selectedTagIdAsGuid);
+                if (existingTag != null)
+                {
+                    selectedTags.Add(existingTag);
+                }
+            }
+            post.Tags = selectedTags;
+
+            await _postRepository.AddAsync(post);
             return RedirectToAction("Add");
         }
     }
--- Blog.Web/Models/ViewModels/PostAddRequest.cs
+++ Blog.Web/Models/ViewModels/PostAddRequest.cs
@@ -1,18 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace Blog.Web.Models.ViewModels;
 
 public class PostAddRequest
 {
-    public string Header { get; set; }
-    public string PageTitle { get; set; }
-    public string Content { get; set; }
-    public string ShortDescription { get; set; }
-    public string FeaturedImageUrl { get; set; }
-    public string UrlHandle { get; set; }
+    [Required]
+    [Display(Name = "Post Header")]
+    public string? Header { get; set; }
+    [Required]
+    [Display(Name = "Post Title")]
+    public string? PageTitle { get; set; }
+    [Required]
+    [Display(Name = "Post Content")]
+    public string? Content { get; set; }
+    [Required]
+    [Display(Name = "Post Short Description")]
+    public string? ShortDescription { get; set; }
+    [Required]
+    [Display(Name = "Post Featured Image Url")]
+    public string? FeaturedImageUrl { get; set; }
+    [Required]
+    [Display(Name = "Post Url Handle")]
+    public string? UrlHandle { get; set; }
+    [Required]
+    [Display(Name = "Post Publish Date")]
     public DateTime PublishedDate { get; set; }
-    public string Author { get; set; }
+    [Required]
+    [Display(Name = "Post Author")]
+    public string? Author { get; set; }
+    [Required]
+    [Display(Name = "Would you like to show this post?")]
     public bool IsVisible { get; set; }
+    [Required]
+    [Display(Name = "Post Tags")]
     public IEnumerable<SelectListItem> Tags { get; set; }
     public string[] SelectedTags { get; set; } = Array.Empty<string>();
 }
