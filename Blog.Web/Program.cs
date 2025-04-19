1e580dcd56468aa078bc79dc443890b92c3b3b25
MahmoodElbadri <mahmoud.elbadry357@gmail.com>
2025-04-19 13:26:42 +0200

    Add PostRepository service registration
    This commit adds a new service registration for `IPostRepository` with its implementation `PostRepository` in the `Program.cs` file. This enhancement allows the application to manage posts in addition to tags, improving its overall functionality.

--- Blog.Web/Program.cs
+++ Blog.Web/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddScoped<ITagRepository, TagRepository>();
+builder.Services.AddScoped<IPostRepository, PostRepository>();
 
 var app = builder.Build();
 
