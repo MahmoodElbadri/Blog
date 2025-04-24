471992c949e9e943700313571c9a3da25f8c35de
MahmoodElbadri <mahmoud.elbadry357@gmail.com>
2025-04-25 01:20:47 +0300

    Add favicon, update styles, and implement AuthDbContext
    - Added a favicon link in `_Layout.cshtml` for improved branding.
    - Removed several CSS rules in `site.css`, affecting branding transitions.
    - Modified `fav.png` (binary file changes).
    - Introduced `AuthDbContext` in `AuthDbContext.cs` for user authentication and role management, defining roles for "Admin", "SuperAdmin", and "User".

---
+++ Blog.Web/Data/AuthDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Web.Data;
+
+public class AuthDbContext : IdentityDbContext
+{
+    public AuthDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        var adminRoleID = "e3cb2ced-99c7-4297-bb8d-25e7f9379d81";
+        var superAdminRoleID = "5000a19e-1049-47cf-a93b-44e252bdff0d";
+        var userRoleID = "efcda308-8649-4985-91cd-95720c28745e";
+        var roles = new List<IdentityRole>()
+        {
+            new IdentityRole
+            {
+                Id = adminRoleID,
+                Name = "Admin",
+                NormalizedName = "ADMIN",
+                ConcurrencyStamp = adminRoleID,
+            },
+            new IdentityRole
+            {
+                Id = superAdminRoleID,
+                Name = "SuperAdmin",
+                NormalizedName = "SUPERADMIN",
+                ConcurrencyStamp = superAdminRoleID,
+            },
+            new IdentityRole
+            {
+                Id = userRoleID,
+                Name = "User",
+                NormalizedName = "USER",
+                ConcurrencyStamp = userRoleID,
+            }
+        };
+    }
+}
