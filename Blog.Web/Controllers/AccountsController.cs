99812e7d4309d0483cd66e98a5e52ae36c898f4d
MahmoodElbadri <mahmoud.elbadry357@gmail.com>
2025-04-26 16:50:06 +0300

    Add user registration functionality and UI updates
    - Updated `AccountsController` to handle user registration asynchronously using `UserManager<IdentityUser>`.
    - Modified `Register.cshtml` to use `asp-action` for form submission and added validation summary.
    - Enhanced `_Layout.cshtml` with a "Register" link in the navigation bar for improved accessibility.
    - Made minor formatting adjustments in the footer section of `_Layout.cshtml`.

--- Blog.Web/Controllers/AccountsController.cs
+++ Blog.Web/Controllers/AccountsController.cs
@@ -1,10 +1,18 @@
 ﻿using Blog.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace Blog.Web.Controllers
 {
     public class AccountsController : Controller
     {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AccountsController(UserManager<IdentityUser> userManager)
+        {
+            this._userManager = userManager;
+        }
         [HttpGet]
         public IActionResult Register()
         {
@@ -12,9 +20,30 @@ namespace Blog.Web.Controllers
         }
 
         [HttpPost]
-        public IActionResult Register(RegisterVM registerVM)
+        public async Task<IActionResult> Register(RegisterVM registerVM)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser()
+                {
+                    UserName = registerVM.Name,
+                    Email = registerVM.Email,
+                };
+                var result = await _userManager.CreateAsync(user, registerVM.Password);
+                if (result.Succeeded)
+                {
+                    var identityRole = await _userManager.AddToRoleAsync(user, "User");
+                    if (identityRole.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(registerVM);
         }
     }
 }
