ace944acd2a7b40efaad902e90245df5da2bd18b
MahmoodElbadri <mahmoud.elbadry357@gmail.com>
2025-04-26 15:56:30 +0300

    Add user registration functionality
    Introduces `AccountsController` with GET and POST methods for user registration.
    Creates `RegisterVM` view model with validation for `Name`, `Email`, and `Password`.
    Adds `Register.cshtml` view for user input and form submission.

---
+++ Blog.Web/Controllers/AccountsController.cs
@@ -0,0 +1,20 @@
+﻿using Blog.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Controllers
+{
+    public class AccountsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterVM registerVM)
+        {
+            return View();
+        }
+    }
+}
---
+++ Blog.Web/Models/ViewModels/RegisterVM.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Web.Models.ViewModels;
+
+public class RegisterVM
+{
+    [Required]
+    [StringLength(50)]
+    [Display(Name = "User Name")]
+    public string? Name { get; set; }
+
+    [Required]
+    [EmailAddress]
+    [StringLength(50)]
+    [Display(Name = "Email Address")]
+    public string? Email { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    [DataType(DataType.Password)]
+    public string? Password { get; set; }
+}
