6064688375bc6e2cb144d10c01f3fe59022c8825
MahmoodElbadri <mahmoud.elbadry357@gmail.com>
2025-04-26 14:15:20 +0300

    Add super admin user and roles to AuthDbContext, fix label syntax in Delete view, and update blog title in Index view

--- Blog.Web/Data/AuthDbContext.cs
+++ Blog.Web/Data/AuthDbContext.cs
@@ -17,6 +17,8 @@ public class AuthDbContext : IdentityDbContext
         var adminRoleID = "e3cb2ced-99c7-4297-bb8d-25e7f9379d81";
         var superAdminRoleID = "5000a19e-1049-47cf-a93b-44e252bdff0d";
         var userRoleID = "efcda308-8649-4985-91cd-95720c28745e";
+        var superAdminId = "F8E1C360-5D8C-4DCF-8F10-AF4724C7BD3E";
+        
         var roles = new List<IdentityRole>()
         {
             new IdentityRole
@@ -41,5 +43,40 @@ public class AuthDbContext : IdentityDbContext
                 ConcurrencyStamp = userRoleID,
             }
         };
+        builder.Entity<IdentityRole>().HasData(roles);
+
+        var superAdminUser = new IdentityUser()
+        {
+            UserName = "superadmin@badri.com",
+            Email = "superadmin@badri.com",
+            NormalizedEmail = "superadmin@badri.com".ToUpper(),
+            NormalizedUserName = "superadmin@badri.com".ToUpper(),
+            Id = superAdminId,
+        };
+
+        superAdminUser.PasswordHash = new PasswordHasher<IdentityUser>()
+            .HashPassword(superAdminUser, "Super@123");
+        
+        builder.Entity<IdentityUser>().HasData(superAdminUser);
+
+        var superAdminRoles = new List<IdentityUserRole<string>>
+        {
+            new IdentityUserRole<string>
+            {
+                RoleId = superAdminRoleID,
+                UserId = superAdminId,
+            },
+            new IdentityUserRole<string>
+            {
+                RoleId = superAdminRoleID,
+                UserId = adminRoleID,
+            },
+            new IdentityUserRole<string>
+            {
+                RoleId = userRoleID,
+                UserId = superAdminId,
+            }
+        };
+        builder.Entity<IdentityUserRole<string>>().HasData(superAdminRoles);
     }
 }
