ccfeab30cb7c7b952e5e6a073028fc4fe22d978d
MahmoodElbadri <mahmoud.elbadry357@gmail.com>
2025-04-28 21:26:04 +0300

    Add admin user management and enhance UI elements
    - Implement authorization for AdminUsersController.
    - Streamline user retrieval in the List method.
    - Enhance Login and Register pages with styled headers.
    - Add a styled header to the Index page.
    - Update layout to include navigation for "All Users."
    - Create List.cshtml to display user details with handling for empty states.

--- Blog.Web/Controllers/AdminUsersController.cs
+++ Blog.Web/Controllers/AdminUsersController.cs
@@ -1,10 +1,12 @@
 ﻿using Blog.Web.Models.Domain;
 using Blog.Web.Models.ViewModels;
 using Blog.Web.Repositories;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Web.Controllers;
 
+[Authorize(Roles = "Admin")]
 public class AdminUsersController : Controller
 {
     private readonly IUsersRepository _usersRepository;
@@ -16,17 +18,12 @@ public class AdminUsersController : Controller
 
     public async Task<IActionResult> List()
     {
-        // Get users from repository
         var users = await _usersRepository.GetAllUsersAsync();
 
-        // Create view model
         var userVM = new UserVM();
         userVM.Users = new List<UserModel>();
-
-        // Map the returned users to UserModel
         foreach (var user in users)
         {
-            // Make sure to map from the correct type (likely IdentityUser)
             userVM.Users.Add(new UserModel
             {
                 Email = user.Email,
